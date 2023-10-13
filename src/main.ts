ba47e4a7e6a543b54f2e84b7d04a7551077c9c34
JVormstein <77570814+JVormstein@users.noreply.github.com>
2023-10-13 14:10:08 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import {Plugin} from 'obsidian';
+import {Plugin, Modal} from 'obsidian';
 import SidePanel, {SIDE_PANEL_ID} from "./SidePanel";
 
 
@@ -8,6 +8,30 @@ export default class ObsiDOOM extends Plugin {
 			SIDE_PANEL_ID, (leaf) => new SidePanel(leaf)
 		);
 
+		this.addCommand({
+			id: 'open-obsidoom',
+			name: 'Open Obsidoom',
+			callback: () => {
+				new OpenObsidoom(this.app).open();
+			}
+		});
+
 		await SidePanel.activate(this.app.workspace);
+	}
+}
+
+class OpenObsidoom extends Modal {
+	constructor(app: App) {
+		super(app);
+	}
+
+	onOpen() {
+		const {contentEl} = this;
+		contentEl.setText('Woah!');
+	}
+
+	onClose() {
+		const {contentEl} = this;
+		contentEl.empty();
 	}
 }
