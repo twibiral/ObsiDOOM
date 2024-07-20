03b922e1c13ef80cf7d2be8045cef6b9aa982fb8
Holo <gfx.holo@proton.me>
2024-07-21 01:11:14 +1000

    feat: add command to reopen game tab

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import SidePanel, {SIDE_PANEL_ID} from "./SidePanel";
 
 export default class ObsiDOOM extends Plugin {
 	async onload() {
+		this.addCommand({
+			id: "open",
+			name: "Open game",
+			callback: () => SidePanel.activate(this.app.workspace)
+		});
+
 		this.registerView(
 			SIDE_PANEL_ID, (leaf) => new SidePanel(leaf)
 		);
